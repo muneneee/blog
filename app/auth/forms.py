e53210629cf2c0e3e745810e88e31acff7557376
[muneneee] <[lukekevin39@gmail.com]>
2020-05-10 17:12:38 +0300

    added view routes for auth

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -21,3 +21,11 @@ class RegistrationForm(FlaskForm):
     EqualTo('password2',message = 'Passwords must match')])
     password2 = PasswordField('Confirm Passwords',validators = [Required()])
     submit = SubmitField('Sign Up')
+
+    def validate_email(self,data_field):
+        if User.query.filter_by(email =data_field.data).first():
+            raise ValidationError('There is an account with that email')
+
+    def validate_username(self,data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError('That username is taken')
