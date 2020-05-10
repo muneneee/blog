5231bb73393432585b15e434cfa9209cdad08ffa
[muneneee] <[lukekevin39@gmail.com]>
2020-05-10 17:33:31 +0300

    added a new blog form

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,7 @@
 from flask import render_template,request,redirect,url_for,abort
 from app.requests import get_quotes
 from . import main
-from .forms import UpdateProfile,PitchForm,CommentForm
+from .forms import BlogForm,CommentForm
 from flask_login import login_required, current_user
 from .. import db
 from ..models import User,Blog,Comment
@@ -30,5 +30,23 @@ def profile(uname):
         abort(404)
 
     return render_template("profile/profile.html", user = user, title = title)
+
+
+
+@main.route('/blog', methods = ['GET','POST'])
+@login_required
+def new_blog():
+    form = BlogForm()
+
+    if form.validate_on_submit():
+        blog_title = form.title.data
+        blog = form.pitch.data
+        new_blog = Blog(blog_title=blog_title,blog = blog)
+        new_pitch.save_blog()
+        return redirect(url_for('main.index'))
+
+
+    title = 'New Blog'
+    return render_template('blog.html' ,title = title, blog_form = form)
 
 
