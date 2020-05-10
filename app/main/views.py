c45d4c7d8ec1c094564042789e26724dd89dff31
[muneneee] <[lukekevin39@gmail.com]>
2020-05-10 17:26:48 +0300

    added profile route

--- app/main/views.py
+++ app/main/views.py
@@ -1,11 +1,34 @@
-from flask import render_template
+from flask import render_template,request,redirect,url_for,abort
 from app.requests import get_quotes
 from . import main
+from .forms import UpdateProfile,PitchForm,CommentForm
+from flask_login import login_required, current_user
+from .. import db
+from ..models import User,Blog,Comment
+
 
 
 @main.route('/')
 def index():
 
 
-    title = 'Kevin Blog'
-    return render_template('index.html',title = title,)
+    blogs = Blog.get_blogs()
+
+
+    title = 'K Blog'
+    return render_template('index.html',title = title, comments = comments)
+
+
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    title = f'{uname} profile'
+
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user, title = title)
+
+
